792125176784baa4ed7fc536384b72789cf2d645
Nael <naelbego@gmail.com>
2021-03-11 21:34:48 +0700

    Update

---
+++ Index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewpoint" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>AYO Berjuang lebih keras</title>
+</head>
+<body>
+	<h2>AYOJANGANPATAH SEMANGAT</h2>
+</body>
+</html>
